1b7342c23ae94172bfeb3fbc3aa3887fd70a538e
Vishwaslad8549 <mstvishwas@gmail.com>
2025-02-04 23:47:35 +0530

    Email js config and contact form UI developed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
-
+import emailjs from 'emailjs-com';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  ismessagesent:boolean=false;
+  isError:boolean=false;
   contactForm = this.fb.group({
     name: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
@@ -17,9 +20,26 @@ export class ContactComponent {
 
   onSubmit() {
     if (this.contactForm.valid) {
-      console.log('Form data:', this.contactForm.value);
-      // Add your submission logic here
-      this.contactForm.reset();
+      emailjs
+        .send(
+          environment.emailjs.serviceId, // Use Service ID from environment
+          environment.emailjs.templateId, // Use Template ID from environment
+          this.contactForm.value, // Form data
+          environment.emailjs.userId // U
+        )
+        .then(
+          (response) => {
+            //console.log('Email sent successfully', response);
+            //alert('Message sent successfully!');
+            this.ismessagesent=true;
+            this.contactForm.reset();
+          },
+          (error) => {
+            console.error('Error sending email', error);
+            this.isError=true
+            //alert('Failed to send message. Please try again.');
+          }
+        );
     }
   }
 }
