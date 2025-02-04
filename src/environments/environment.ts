b2080eb8fd4f6dcd6f1781c7f31020c22501a5c0
Vishwaslad8549 <mstvishwas@gmail.com>
2025-02-05 00:03:25 +0530

    changed gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    emailjs: {
+      serviceId: 'service_52tw97h', // Replace with your Service ID
+      templateId: 'template_gz4eqzy', // Replace with your Template ID
+      userId: 'HatGdn0Nxfp7u8ONg', // Replace with your User ID
+    },
+  };
