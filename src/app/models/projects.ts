321f5c18c44fe0fa0594216b242e62d984688b81
Vishwaslad8549 <mstvishwas@gmail.com>
2025-02-05 11:19:57 +0530

    Project section

--- src/app/models/projects.ts
+++ src/app/models/projects.ts
@@ -1,5 +1,5 @@
 export interface Project{
-image:string;
+images:string[];
 title:string;
 description:string;
 technologies:string[];
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,16 +12,45 @@ export class ProjectsComponent {
       title: 'E-commerce Platform',
       description: 'Full-featured Angular e-commerce solution with NgRx state management',
       technologies: ['Angular', 'NgRx', 'Firebase', 'SCSS'],
-      image: 'assets/images/project1.jpg',
+      images: ['assets/Postify_Login.png', 'assets/project1-2.jpg', 'assets/project1-3.jpg'],
       link: '#'
     },
     {
       title: 'Real-time Dashboard',
       description: 'Data visualization dashboard with real-time updates using WebSocket',
       technologies: ['Angular', 'RxJS', 'Chart.js', 'Node.js'],
-      image: 'assets/images/project2.jpg',
+      images: ['assets/Postify_Login.png', 'assets/project1-2.jpg', 'assets/project1-3.jpg'],
       link: '#'
     },
     // Add more projects
   ];
+  isSliderOpen = false;
+  selectedProjectIndex = 0;
+  selectedImageIndex = 0;
+
+  openSlider(projectIndex: number) {
+    this.selectedProjectIndex = projectIndex;
+    this.selectedImageIndex = 0;
+    this.isSliderOpen = true;
+  }
+
+  closeSlider() {
+    this.isSliderOpen = false;
+  }
+
+  prevImage() {
+    if (this.selectedImageIndex > 0) {
+      this.selectedImageIndex--;
+    } else {
+      this.selectedImageIndex = this.projects[this.selectedProjectIndex].images.length - 1;
+    }
+  }
+
+  nextImage() {
+    if (this.selectedImageIndex < this.projects[this.selectedProjectIndex].images.length - 1) {
+      this.selectedImageIndex++;
+    } else {
+      this.selectedImageIndex = 0;
+    }
+  }
 }
